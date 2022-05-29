fc2dd5b8dd88bb3d9ecbfa2b3cb6500836d2e836
Subin Pyeon <sbp111@naver.com>
2022-05-29 15:38:28 +0900

    지도 액티비티 구현까지

--- app/src/main/java/com/example/fighting/MainActivity.java
+++ app/src/main/java/com/example/fighting/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.fighting;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_map = (Button) findViewById(R.id.main_button3);
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Walk.class);
+                startActivity(intent);
+            }
+        });
     }
 }

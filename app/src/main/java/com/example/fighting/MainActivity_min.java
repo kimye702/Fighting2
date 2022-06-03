6db55b44ed659f9efd8c023cf5dceab6fd05e054
Oasisday <a95653090@gamil.com>
2022-06-03 15:08:19 +0900

    final_cal

---
+++ app/src/main/java/com/example/fighting/MainActivity_min.java
@@ -0,0 +1,85 @@
+package com.example.fighting;
+
+import static com.example.cal_project1.CalendarUtils.daysInMonthArray;
+import static com.example.cal_project1.CalendarUtils.monthYearFromDate;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity_min extends AppCompatActivity implements CalenderAdapter.OnItemListener{
+
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    LocalDate selectedDate;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_min);
+        initWidgets();
+        selectedDate = LocalDate.now();
+        CalendarUtils.selectedDate = LocalDate.now();
+        setMonthView();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setMonthView() {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
+
+        CalenderAdapter calenderAdapter = new CalenderAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calenderAdapter);
+    }
+
+
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void previousMonthAction(View view){
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void nextMonthAction(View view){
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onItemClick(int position, LocalDate date) {
+        if(date!=null) {
+            CalendarUtils.selectedDate = date;
+            setMonthView();
+        }
+        startActivity(new Intent(this, WeekViewActivity.class));
+    }
+
+}
